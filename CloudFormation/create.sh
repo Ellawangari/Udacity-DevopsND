9558d73e3395db746a5504cde256381694078a28
Ellawangari <elamaina8@gmail.com>
2022-07-15 11:03:49 +0300

    added scripts

---
+++ CloudFormation/create.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name Stack1 --template-body file://network.yaml  --parameters file://network-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
