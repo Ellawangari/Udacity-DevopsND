62fa6926bd1ef1cf677bc95ca4f863c1d46e11c7
Ellawangari <elamaina8@gmail.com>
2022-07-17 22:37:33 +0300

    added files

--- CloudFormation/create.sh
+++ CloudFormation/create.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name Stack1 --template-body file://network.yaml  --parameters file://network-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation create-stack --stack-name Stack2 --template-body file://webservers.yaml  --parameters file://webservers-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
